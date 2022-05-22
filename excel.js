9a4955c09e2fc1811e5985749d5dc822dd9dff04
RiyaJha <78296094+riyajha2305@users.noreply.github.com>
2022-05-22 10:59:52 +0530

    Add files via upload

---
+++ excel.js
@@ -0,0 +1,22 @@
+let selectedFile;
+
+document.getElementById('input').addEventListener("change",(event)=>{
+    selectedFile = event.target.files[0];
+})
+document.getElementById('button').addEventListener("click",()=>{
+    if(selectedFile){
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=>{
+            console.log(event.target.result);
+            let data = event.target.result;
+            let workbook = XLSX.read(data,{type:"binary"});
+            console.log(workbook);
+            workbook.SheetNames.forEach(sheet => {
+                let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+                console.log(rowObject);
+                document.getElementById('jsondata').innerHTML = JSON.stringify(rowObject,undefined,4);
+            });
+        }
+    }
+})
